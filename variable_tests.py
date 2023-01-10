ebb5175b26e084571159d53e63402a41221f8d24
Valentin Nimmervoll <valnim46@gmail.com>
2023-01-10 19:10:42 +0100

    Add basic Variable test file

---
+++ variable_tests.py
@@ -0,0 +1,108 @@
+import numetricpy as nmp
+
+# SI units
+m = nmp.UnitValue(1, 'm')
+s = nmp.UnitValue(1, 's')
+kg = nmp.UnitValue(1, 'kg')
+A = nmp.UnitValue(1, 'A')
+K = nmp.UnitValue(1, 'K')
+mol = nmp.UnitValue(1, 'mol')
+cd = nmp.UnitValue(1, 'cd')
+
+
+# Multiples of SI units
+# Length Units
+mm = nmp.UnitValue(1, 'mm')
+cm = nmp.UnitValue(1, 'cm')
+dm = nmp.UnitValue(1, 'dm')
+km = nmp.UnitValue(1, 'km')
+
+# Mass Units
+mg = nmp.UnitValue(1, 'mg')
+g = nmp.UnitValue(1, 'g')
+
+# Time Units
+ms = nmp.UnitValue(1, 'ms')
+minute = nmp.UnitValue(1, 'min')
+h = nmp.UnitValue(1, 'h')
+
+# Temperature Units
+degC = nmp.UnitValue(1, '°C')
+degF = nmp.UnitValue(1, '°F')
+
+# Current Units
+mA = nmp.UnitValue(1, 'mA')
+kA = nmp.UnitValue(1, 'kA')
+MA = nmp.UnitValue(1, 'MA')
+
+# Amount of Substance Units
+kmol = nmp.UnitValue(1, 'kmol')
+
+
+# Derived Units
+# Frequency Units
+Hz = nmp.UnitValue(1, 'Hz')
+
+# Angle Units
+rad = nmp.UnitValue(1, 'rad')
+
+# Solid Angle Units
+sr = nmp.UnitValue(1, 'sr')
+
+# Force Units
+N = nmp.UnitValue(1, 'N')
+kN = nmp.UnitValue(1, 'kN')
+
+# Pressure Units
+Pa = nmp.UnitValue(1, 'Pa')
+bar = nmp.UnitValue(1, 'bar')
+
+# Energy Units
+J = nmp.UnitValue(1, 'J')
+eV = nmp.UnitValue(1, 'eV')
+cal = nmp.UnitValue(1, 'cal')
+
+# Power Units
+W = nmp.UnitValue(1, 'W')
+hp = nmp.UnitValue(1, 'hp')
+
+# Charge Units
+C = nmp.UnitValue(1, 'C')
+
+# Voltage Units
+V = nmp.UnitValue(1, 'V')
+
+# Capacitance Units
+F = nmp.UnitValue(1, 'F')
+
+# Resistance Units
+ohm = nmp.UnitValue(1, 'ohm')
+
+# Conductance Units
+S = nmp.UnitValue(1, 'S')
+
+# Magnetic Flux Units
+Wb = nmp.UnitValue(1, 'Wb')
+
+# Magnetic field strength Units
+T = nmp.UnitValue(1, 'T')
+
+# Inductance Units
+H = nmp.UnitValue(1, 'H')
+
+# Luminous Flux Units
+lm = nmp.UnitValue(1, 'lm')
+
+# Illuminance Units
+lx = nmp.UnitValue(1, 'lx')
+
+# Activity (of a radionuclide) Units
+Bq = nmp.UnitValue(1, 'Bq')
+
+# Absorbed dose Units
+Gy = nmp.UnitValue(1, 'Gy')
+
+# Equivalent dose Units
+Sv = nmp.UnitValue(1, 'Sv')
+
+
