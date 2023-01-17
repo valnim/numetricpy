85a5ce85905fbbc179c047628003fcd23ec445ed
Valentin Nimmervoll <valnim46@gmail.com>
2023-01-17 22:38:31 +0100

    Example test script for UnitConstants

---
+++ example_UnitConstants.py
@@ -0,0 +1,58 @@
+from numetricpy import UnitConstants
+
+# Call all constants in UnitConstants
+pi = UnitConstants.pi
+e = UnitConstants.e
+
+c = UnitConstants.c
+
+eV = UnitConstants.eV
+mu_0 = UnitConstants.mu_0
+epsilon_0 = UnitConstants.eps_0
+k_c = UnitConstants.k_c
+Z_0 = UnitConstants.Z_0
+
+h = UnitConstants.h
+hbar = UnitConstants.hbar
+Phi_0 = UnitConstants.Phi_0
+G_0 = UnitConstants.G_0
+R_k = UnitConstants.R_k
+K_j = UnitConstants.K_j
+alpha = UnitConstants.alpha
+
+k = UnitConstants.k
+k_b = UnitConstants.k_b
+sigma = UnitConstants.sigma
+c_1 = UnitConstants.c_1
+c_2 = UnitConstants.c_2
+b = UnitConstants.b
+
+G = UnitConstants.G
+g_earth = UnitConstants.g_earth
+m_planck = UnitConstants.m_planck
+l_planck = UnitConstants.l_planck
+t_planck = UnitConstants.t_planck
+T_planck = UnitConstants.T_planck
+
+m_e = UnitConstants.m_e
+lambda_c = UnitConstants.lambda_c
+r_e = UnitConstants.r_e
+sigma_e = UnitConstants.sigma_e
+mu_b = UnitConstants.mu_b
+mu_e = UnitConstants.mu_e
+g_e = UnitConstants.g_e
+gamma_e = UnitConstants.gamma_e
+
+a_0 = UnitConstants.a_0
+R_inf = UnitConstants.R_inf
+R_inf_c = UnitConstants.R_inf_c
+R_inf_hc = UnitConstants.R_inf_hc
+E_h = UnitConstants.E_h
+
+N_A = UnitConstants.N_A
+F = UnitConstants.F
+R = UnitConstants.R
+
+
+
+
